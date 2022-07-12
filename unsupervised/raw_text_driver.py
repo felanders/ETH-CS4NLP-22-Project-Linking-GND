0e0c637d83c37299858034ed5d169450e93614f7
fatih <afeserr@gmail.com>
2022-07-12 23:15:40 +0200

    implement local context

--- unsupervised/raw_text_driver.py
+++ unsupervised/raw_text_driver.py
@@ -36,3 +36,43 @@ def get_context_vectors(file_name, word2vec, raw_data_path):
             skipped = skipped + 1
     print(f'Number of context vectors from text {total-skipped}/{total}')
     return np.array(vectors)
+
+def get_context_vectors(file_name, location: str, word2vec, raw_data_path, window_size):
+    """
+    assume the raw text information is: {'page': 'dkm-003_2010_070_0048.txt', 'coords': '1353,2120,113,29'}
+
+    @param file_name: dkm-003_2010_070_0048.txt
+    @param location: 1353,2120
+    @param word2vec: glove dictionary
+    @param raw_data_path: path to search for
+    @param window_size: size to calculate the context information
+    """
+
+    path = get_file_path(file_name=file_name, raw_data_path=raw_data_path)
+    with open(path, 'r') as my_file:
+        lines = my_file.readlines()
+
+    vectors = []
+    total = 0
+    skipped = 0
+    # find the location of the target!
+    line_indices = []
+    for counter, line in enumerate(lines):
+        if location in line:
+            # from -window_size to +window_size
+            for counter2 in range(-window_size, window_size):
+                line_indices.append(counter + counter2)
+
+    for line_counter in line_indices:
+        line = lines[line_counter]
+        splitted = line.split(' ')
+        if len(splitted) == 1:
+            continue
+        # print('word is', splitted[0])
+        try:
+            total = total + 1
+            vectors.append(word2vec[splitted[0]])
+        except KeyError:
+            skipped = skipped + 1
+    print(f'Number of context vectors from text {total-skipped}/{total}')
+    return np.array(vectors)
