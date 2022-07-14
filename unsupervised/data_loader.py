fix distance calculation bug

unsupervised/data_loader.py
@@ -15,8 +15,8 @@ from unsupervised.raw_text_driver import get_context_vectors
 # PATH_ENTITY_MAP = 'data/entity_map_lookup.pickle'
 
 different_name_for_logging.basicConfig()
-logger = different_name_for_logging.getLogger()
-logger.setLevel(different_name_for_logging.DEBUG)
+logger = different_name_for_logging.getLogger(__file__)
+logger.setLevel(different_name_for_logging.WARNING)
 
 print('Available models:', list(gensim.downloader.info()['models'].keys()))
 
@@ -98,9 +98,9 @@ class DataLoader:
             # print('source', source_word_vec)
             # print('other', candidate_document_vectors[counter, :])
             if similarity_measure == 'distance':
-                distances.append([np.linalg.norm(source_word_vec - candidate_document_vectors[counter, counter2, :]) for counter2 in range(num_features)])
+                distances.append([np.linalg.norm(source_word_vec[counter2, :] - candidate_document_vectors[counter, counter2, :]) for counter2 in range(num_features)])
             elif similarity_measure == 'cosine_similarity':
-                distances.append([spatial.distance.cosine(source_word_vec - candidate_document_vectors[counter, counter2, :]) for counter2 in range(num_features)])
+                distances.append([spatial.distance.cosine(source_word_vec[counter2, :], candidate_document_vectors[counter, counter2, :]) for counter2 in range(num_features)])
             else:
                 pass
 

unsupervised/raw_text_driver.py
@@ -1,9 +1,13 @@
 """
 Here I calculate the vectors for the input text.
 """
-import logging
+import logging as different_name_for_logging
 import numpy as np
 import os
+
+different_name_for_logging.basicConfig()
+logger = different_name_for_logging.getLogger(__file__)
+logger.setLevel(different_name_for_logging.WARNING)
 
 def get_file_path(file_name, raw_data_path):
     """
@@ -34,7 +38,7 @@ def get_context_vectors(file_name, word2vec, raw_data_path):
             vectors.append(word2vec[splitted[0]])
         except KeyError:
             skipped = skipped + 1
-    logging.debug(f'Number of context vectors from text {total-skipped}/{total}')
+    logger.debug(f'Number of context vectors from text {total-skipped}/{total}')
     return np.array(vectors)
 
 def get_context_vectors(file_name, location: str, word2vec, raw_data_path, window_size):
@@ -80,5 +84,5 @@ def get_context_vectors(file_name, location: str, word2vec, raw_data_path, windo
             vectors.append(word2vec[splitted[0]])
         except KeyError:
             skipped = skipped + 1
-    logging.debug(f'Number of context vectors from text {total-skipped}/{total}')
+    logger.debug(f'Number of context vectors from text {total-skipped}/{total}')
     return np.array(vectors)